f9561759b5a449905be3605937fce388e51e82ad
John Setzer <jsetzer@yammer-inc.com>
2014-09-25 01:44:17 -0700

    Example with file piping

---
+++ examples/all_metrics_task.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var _ = require('lodash');
+var RR = require('../index.js');
+var matchCounter = require('./rr_match_counter');
+
+gulp.task('default', function(cb) {
+  var rr = new RR({
+  	key: 'deprecatedFunc',
+  	paths: ['../lib/*.js'],
+
+    // Chance to calculate your own total metrics before RR flushes them to the database.
+    syntheticTotals: function (totalMetrics, cb) {
+      // totalMetrics automatically includes:
+      // 'deprecatedFunc.fileCount'
+      // 'deprecatedFunc.nonZeroFileCount'
+      // 'deprecatedFunc.sum'
+      // 'deprecatedFunc.mean'
+      // 'deprecatedFunc.median'
+      // 'deprecatedFunc.min'
+      // 'deprecatedFunc.max'
+      // 'deprecatedFunc.stdDev'
+
+      // At this point one could caluculate synthetic values such as:
+      //totalMetrics['deprecatedFunc.filePercentage'] = totalMetrics['deprecatedFunc.nonZeroFileCount'] / totalMetrics['deprecatedFunc.fileCount'] * 100;
+      // Lots of the places you might copy this data might 
+      // be better places to caluculate synthetic values on an ad-hoc basis.
+      cb();
+    },
+
+    // Called before RR exits
+  	done: function (err, totalMetrics, cb) {
+	  	// Log to console
+	  	_(totalMetrics).forEach(function (v, k) {
+		  	console.log(k, v);
+		  });
+
+  		// Send metrics somewhere useful.
+  		// Example: Wavefront, Ganglia, SumoLogic, Splunk, etc...
+	  	cb();
+	  }
+	});
+
+  rr.src({ buffer: false })
+  	.pipe(matchCounter(rr.helper, 'console.log'))
+    .pipe(rr.dest(cb));
+});
+
---
+++ examples/rr_match_counter.js
@@ -0,0 +1,56 @@
+// THIS SHOULD BE OWN MODULE ONE DAY
+
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var LineStream = require('byline').LineStream;
+
+// consts
+const PLUGIN_NAME = 'gulp-rr-match-counter';
+
+// plugin level function (dealing with files)
+function matchCounter(rrHelper, searchString) {
+  var patternCount = 0;
+  var pattern = new RegExp(searchString, 'g');
+
+  if (!pattern) {
+    throw new PluginError(PLUGIN_NAME, 'Missing pattern.');
+  }
+
+  // creating a stream through which each file will pass
+  var stream = through.obj(function(file, enc, cb) {
+    if (file.isBuffer()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
+      return cb();
+    }
+
+    if (file.isStream()) {
+      var lineStream = new LineStream();
+      
+      lineStream.on('data', function(line) {
+        var line = line.toString();
+        var match = line.match(pattern);
+        if (match) {
+          patternCount += match.length;
+        }
+
+      });
+
+      lineStream.on('end', function() {
+        console.log('patternCount', patternCount);
+        cb();
+      });
+
+      lineStream.on('error', this.emit.bind(this, 'error'));
+      
+      file.contents.pipe(lineStream);
+    }
+
+    this.push(file);
+    
+  });
+
+  return stream;
+};
+
+module.exports = matchCounter;
